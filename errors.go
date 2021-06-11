9167e52aeab2006656218252d5edf79d82d6a34d
Pavel Korotkov <pkorotkov@gmail.com>
2021-06-12 01:45:57 +0300

    Reformat layout

--- errors.go
+++ errors.go
@@ -1,12 +1,12 @@
 package tractor
 
+const (
+	ErrActorNotFound    = _Error("actor not found")
+	ErrFullActorMailbox = _Error("actor mailbox is full")
+)
+
 type _Error string
 
 func (e _Error) Error() string {
 	return string(e)
 }
-
-const (
-	ErrActorNotFound    = _Error("actor not found")
-	ErrFullActorMailbox = _Error("actor mailbox is full")
-)
